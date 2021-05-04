9bad008534664d5dfba98553ee3a1c1fb844b096
AlexSimakhin <alexsimahin@gmail.com>
2021-05-04 10:50:22 +0300

    init: (init code - 1st day)

---
+++ server/src/track/track.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable()
+
+export class trackService {
+
+  async create () {
+    
+  }
+
+  async getAll() {
+
+  }
+
+  async getOne() {
+
+  }
+
+  async delete() {
+
+  }
+}
